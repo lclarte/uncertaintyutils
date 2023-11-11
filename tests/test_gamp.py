90dd1812c50e6ed8d3c3dc3fd3d6f43e8e9df84f
Lucas Clarte <lucas.clarte@epfl.ch>
2023-11-11 21:26:06 +0100

    - Fixed laplace prior : f_a = Sigma * partial_log + R

--- tests/test_gamp.py
+++ tests/test_gamp.py
@@ -1,11 +1,16 @@
 import numpy as np
 from time import time
 
+import matplotlib.pyplot as plt
+import sklearn.linear_model as linear_model
+
 import uncertaintyutils as uu
 import uncertaintyutils.data as data
 import uncertaintyutils.gamp.gamp as gamp
 import uncertaintyutils.gamp.likelihood as likelihood
 import uncertaintyutils.gamp.prior as prior
+import uncertaintyutils.gamp.prior.laplace_prior as laplace_prior
+import uncertaintyutils.gamp.prior.lasso_prior as lasso_prior
 
 def test_gamp_retrain():
     d = 128
@@ -44,5 +49,65 @@ def test_gamp_retrain():
     print(f'Distance between retrain and whole is {np.linalg.norm(what_retrain - what_whole_dataset)}, norms are {np.linalg.norm(what_retrain)}, {np.linalg.norm(what_whole_dataset)}')
     print(np.linalg.norm(vhat_retrain - vhat_whole_dataset))
 
+def test_gamp_lasso():
+    """
+    Test the lasso prior
+    """
+    d = 50
+    n = 100
+    wstar = data.sample_teacher(np.eye(d))
+    x = np.random.normal(0.0, 1.0, size=(n, d)) / np.sqrt(d)
+    y = x @ wstar
+
+    lambda_lasso = 1.0 / n
+
+    lr_lasso = linear_model.Lasso(alpha=lambda_lasso, fit_intercept=False)
+    lr_lasso.fit(x, y)
+
+    prior_ = lasso_prior.LassoPrior(lambda_ = 1.0)
+    likelihood_ = likelihood.gaussian_log_likelihood.GaussianLogLikelihood(noise_variance=1.0)
+
+    result = gamp.iterate_gamp(x, y, None, likelihood_, prior_, max_iter=10, tol=1e-4, verbose=True)
+    what, vhat = result['estimator'], result['variances']
+    print(f'Correlation between AMP and Lasso is {lr_lasso.coef_ @ what / np.linalg.norm(lr_lasso.coef_) / np.linalg.norm(what)}')
+    plt.scatter(lr_lasso.coef_, what)
+    plt.xlabel('Lasso')
+    plt.ylabel('AMP')
+    plt.show()
+
+def test_gamp_laplace():
+    d = 100
+    noise_std = 1.0
+    wstar = np.random.laplace(0.0, 1.0, size=d)
+    rho = np.linalg.norm(wstar)**2 / d
+
+    n_list = list(map(int, np.linspace(50, 1000, 30)))
+    q_list, m_list, v_list = [], [], []
+    
+    for n in n_list:
+        x = np.random.normal(0.0, 1.0, size=(n, d)) / np.sqrt(d)
+        y = x @ wstar + noise_std * np.random.normal(0.0, 1.0, size=n)
+
+        # prior_ = laplace_prior.LaplacePrior(lambda_ = 1.0)
+        prior_ = lasso_prior.LassoPrior(lambda_ = 1.0)
+        likelihood_ = likelihood.gaussian_log_likelihood.GaussianLogLikelihood(noise_variance=noise_std**2)
+
+        result = gamp.iterate_gamp(x, y, None, likelihood_, prior_, max_iter=50, tol=1e-4, verbose=False)
+        what, vhat = result['estimator'], result['variances']
+
+        q_list.append(what @ what / d)
+        m_list.append(what @ wstar / d)
+        v_list.append(np.mean(vhat))
+
+    plt.plot(n_list, q_list, label='q')
+    plt.plot(n_list, m_list, label='m')
+    plt.plot(n_list, v_list, label='v')
+    plt.legend()
+    plt.show()
+
+    plt.plot(n_list, np.array(m_list) / np.sqrt(q_list) / np.sqrt(rho))
+    plt.show()
+
+
 if __name__ == "__main__":
-    test_gamp_retrain()
+    test_gamp_laplace()
--- uncertaintyutils/gamp/prior/laplace_prior.py
+++ uncertaintyutils/gamp/prior/laplace_prior.py
@@ -16,7 +16,7 @@ def ddR_log_Z(Sigma, R, lambda_):
     tmp_pi = np.sqrt(2.0 / np.pi)
     Rm, Rp = R - lambda_ * Sigma, R + lambda_ * Sigma
     return 2 * lambda_ * tmp_exp * ( - np.exp(-Rp**2 / tmp**2) * tmp_pi + ( 2 * lambda_ * np.sqrt(Sigma) - tmp_pi * np.exp(-Rm**2 / tmp**2)) * erfc(Rp / tmp) + \
-            erf(Rm / tmp) * (2 * lambda_ * np.sqrt(Sigma) * erfc(Rp / tmp) - np.exp(-Rp**2 / tmp**2) * tmp_pi )) / \
+            erf(Rm / tmp) * (2 * lambda_ * np.sqrt(Sigma) * erfc(Rp / tmp) - np.exp(-Rp**2 / tmp**2) * tmp_pi)) / \
             (np.sqrt(Sigma) * (1.0 + erf(Rm / tmp) + tmp_exp * erfc(Rp / tmp) )**2)
     
 class LaplacePrior:
@@ -25,22 +25,19 @@ class LaplacePrior:
         self.lambda_ = lambda_
     
     def fa(self, Sigma : float, R : float) -> float:
-        """
-        Input function, independent of the variance of gaussian prior
-        NOTE : Should not depend on the noise in label
-        """
-        return Sigma * (dR_log_Z(Sigma, R, self.lambda_) + R)
+        # Seems to be dR_log_Z(Sigma, R, self.lambda_) but why is it not Sigma * (dR_log_Z(Sigma, R, self.lambda_) + R) ? Maybe the pdf is wrong
+        # return dR_log_Z(Sigma, R, self.lambda_)
+        return Sigma * dR_log_Z(Sigma, R, self.lambda_) + R
 
     def fv(self, Sigma : float, R : float) -> float:
         """
         Derivative of input function w.r.t. R, multiplied by Sigma
         """
-        return Sigma**2 * (ddR_log_Z(Sigma, R, self.lambda_) + 1.0)
+        # return Sigma * ddR_log_Z(Sigma, R, self.lambda_)
+        return Sigma**2 * ddR_log_Z(Sigma, R, self.lambda_) + Sigma
     
     def prior(self, b : float, A : float):
         '''
-        Compute f and f' for Bernoulli-Gaussian prior
-        
         Sigma = 1 / A
         R = b / A
         '''
