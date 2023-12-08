fcb8ebe74023066a0806252b69d98640b7b44892
Lucas Clarte <lucas.clarte@epfl.ch>
2023-12-08 10:11:48 +0100

    - added functions to model different resamples

--- uncertaintyutils/gamp/gamp_weighted_loss_se.py
+++ uncertaintyutils/gamp/gamp_weighted_loss_se.py
@@ -16,11 +16,38 @@ def dz0(y, omega, v, noise_variance):
         return (y - omega) / (v + noise_variance) * np.exp(- 0.5 * (y - omega).powi(2) / (noise_variance + v)) / np.sqrt(2.0 * np.pi * (noise_variance + v))
 """
 
+# Function to model the different kind of resamples
+
+weights_proba_function_bootstrap = lambda w1, w2 : stats.poisson.pmf(w1, mu=1.0) * stats.poisson.pmf(w2, mu=1.0)
+
+
+weights_proba_function_full_resample = lambda w1, w2 : 0.5 if (w1 == 1.0 and w2 == 0.0) or (w1 == 0.0 and w2 == 1.0) else 0.0
+# with this proba functio we'll look at the correlation between the average of bootstrap and ERM
+# the second estimator will be ERM so the proba is 0 if w2 != 1
+weights_proba_function_bootstrap_erm = lambda w1, w2 : stats.poisson.pmf(w1, mu=1.0) if w2 == 1.0 else 0.0
+
+def get_weights_proba_function_cross_validation(k):
+    assert k >= 2, "k must be >= 2"
+    def weights_proba_function(w1, w2):
+        if w1 != w2:
+            return 1.0 / k
+        # the case w1 == w2 == 0.0 is because we model k estimators but run the state evolution for 2 of them 
+        elif w1 == w2 == 0.0:
+            return 1.0 - 2.0 / k
+        else:
+            return 0.0
+    return weights_proba_function
+
+## 
+
+
 def update_hatoverlaps_fixed_weights(m_vec, q_mat, v_mat, rho_float, weights_vec, teacher_noise_variance):
     """
     we update without the alpha so we'll need to multiply later
     # NOTE : Here we assume the student noise variance is 1,
     otherwise we can divide the weights_vec -> weights_vec / noise_variance
+    # NOTE 2 : Having a student_noise variance different from 1 is useless because
+    we can just rescale the regularization by the student noise variance
     """
     k = len(weights_vec)
     inv_q_mat   = np.linalg.inv(q_mat)
@@ -36,11 +63,18 @@ def update_hatoverlaps_fixed_weights(m_vec, q_mat, v_mat, rho_float, weights_vec
 
     return mhat_vec, qhat_mat, vhat_mat
 
-def update_hatoverlaps(m_vec, q_mat, v_mat, rho_float, alpha, teacher_noise_variance):
+def update_hatoverlaps(m_vec, q_mat, v_mat, rho_float, alpha, teacher_noise_variance, 
+                        weights_bound_1, weights_bound_2, weights_proba_function):
     """
     Here, gout -> vector, dgout -> matrix and we need to integrate over the 
     sampling weights
     NOTE : For now we only to the Poisson case, k = 2
+
+    weights_proba_function : takes two integers as an argument and returns a proba.
+    For bootstrap w/ independent resamples : 
+        weights_proba_function = lambda w1, w2 : stats.poisson.pmf(w1, mu=1.0) * stats.poisson.pmf(w2, mu=1.0)
+    For full resample of X, y : 
+        weights_proba_function = lambda w1, w2 : 0.5 if w1 != w2 else 0.0
     """
     assert len(m_vec) == 2, "Only implemented for k = 2"
 
@@ -48,11 +82,10 @@ def update_hatoverlaps(m_vec, q_mat, v_mat, rho_float, alpha, teacher_noise_vari
     qhat_mat = np.zeros((2, 2))
     vhat_mat = np.zeros((2, 2))
 
-    bound = 5
-    for w1 in range(bound):
-        for w2 in range(bound):
+    for w1 in range(weights_bound_1):
+        for w2 in range(weights_bound_2):
             weights_vec = np.array([w1, w2])
-            proba = stats.poisson.pmf(w1, mu=1.0) * stats.poisson.pmf(w2, mu=1.0)
+            proba = weights_proba_function(w1, w2)
             tmp_mhat_vec, tmp_qhat_mat, tmp_vhat_mat = update_hatoverlaps_fixed_weights(m_vec, q_mat, v_mat, rho_float, weights_vec, teacher_noise_variance)
             mhat_vec += proba * tmp_mhat_vec
             qhat_mat += proba * tmp_qhat_mat
@@ -73,22 +106,31 @@ def update_overlaps(hat_m_vec, hat_q_mat, hat_v_mat, lambda_ridge):
 
     return m_vec, q_mat, v_mat
 
-def state_evolution(alpha, lambda_ridge, teacher_noise_variance, max_iter=100, tol=1e-5):
+def state_evolution(alpha, lambda_ridge, teacher_noise_variance, max_iter=100, tol=1e-5, weights_bound_1=5, weights_bound_2=5, weights_proba_function=weights_proba_function_bootstrap):
+    """
+    Note : weight_bound is NOT included in the weights, e.g. if weight_bound = 5, the max weight is 4
+    so for full resample we need to set weight_bound = 2
+    """
     q_mat = 0.99 * np.eye(2)
     m_vec = np.array([0.1, 0.1])
     v_mat = np.eye(2) + 0.01 * np.ones((2, 2))
     rho_float = 1.0
 
-    qhat_old = np.zeros((2, 2))
+    q_old = np.zeros((2, 2))
 
     for t in range(max_iter):
-        hat_m_vec, hat_q_mat, hat_v_mat = update_hatoverlaps(m_vec, q_mat, v_mat, rho_float, alpha, teacher_noise_variance)
+        hat_m_vec, hat_q_mat, hat_v_mat = update_hatoverlaps(m_vec, q_mat, v_mat, rho_float, alpha, teacher_noise_variance, 
+                                                            weights_bound_1=weights_bound_1, weights_bound_2=weights_bound_2, weights_proba_function=weights_proba_function)
         m_vec, q_mat, v_mat = update_overlaps(hat_m_vec, hat_q_mat, hat_v_mat, lambda_ridge)
 
-        diff = np.max(np.abs(qhat_old - hat_q_mat))
+        diff = np.max(np.abs(q_old - q_mat))
         if diff < tol:
             break
 
-        qhat_old = hat_q_mat.copy()
+        q_old = q_mat.copy()
+
+    if t == max_iter - 1:
+        print(f"Warning : state evolution did not converge, reached {max_iter} iterations, {diff=}")
 
     return m_vec, q_mat, v_mat, hat_m_vec, hat_q_mat, hat_v_mat
+
